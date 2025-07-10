5efc4ffcb35f5d8770222d968a503ca5308510a8
PubliusVergilius <vinteesetedemaiodedoismil@gmail.com>
2025-07-10 00:09:30 -0300

    Health probes

--- api/api/api.go
+++ api/api/api.go
@@ -13,7 +13,13 @@ func init () {
 	log.Printf("Starting new Gin server...")
 }
 
-func Router (port string) {
+type Probes struct {
+	Delay bool
+	Liveness bool
+	Readiness bool
+}
+
+func Router (port string, probes Probes) {
 	router := gin.Default()
 
 	router.GET("/ping", func(ctx *gin.Context) {
@@ -27,9 +33,23 @@ func Router (port string) {
 		log.Printf("Error on getting hostname: %q", err)
 	}
 	router.GET("/hostname", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"hostname": hostname,
-		})	
+	ctx.JSON(http.StatusOK, gin.H{
+		"hostname": hostname,
+	})	
+})
+
+	router.GET("/ready", func (ctx *gin.Context) {
+		if (probes.Readiness) {
+			ctx.AbortWithStatus(503)
+		}
 	})
+
+	router.GET("/healthy", func (ctx *gin.Context) {
+		if (probes.Liveness) {
+			ctx.AbortWithStatus(503)
+		}
+
+	})
+
 	router.Run(fmt.Sprintf(":%s", port))}
 
--- api/cmd/myapi/main.go
+++ api/cmd/myapi/main.go
@@ -2,54 +2,42 @@ package main
 
 import (
 	"api/api"
-	"fmt"
 	"log"
-	"net/http"
+	"math/rand"
 	"os"
 	"strconv"
+	"time"
 )
 
-func handler1(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello from server 1")
-}
-
-func handler2(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello from server 2")
-}
+var delay_startup = true
+var fail_liveness = true
+var fail_readiness = true
 
 func main() {
     // Start Gin server
     port := os.Getenv("API_PORT")
+    delay_startup, err := strconv.ParseBool(os.Getenv("DELAY_STARTUP"))
+    fail_liveness, err := strconv.ParseBool(os.Getenv("FAIL_LIVENESS"))
+    fail_readiness, err := strconv.ParseBool(os.Getenv("FAIL_READINESS")) 
+    if err != nil {
+	    log.Printf("Fail to parse boolean value from env. variabel: %q", err)
+    }
+    fail_readiness = fail_readiness == true && rand.Float64() < 0.5
+    log.Printf("Delay startup: %t", delay_startup)
+    log.Printf("Fail liveness: %t", fail_liveness)
+    log.Printf("Fail readiness: %t", fail_readiness)
 
     // Verify if port range or format is correct
-    _, err := strconv.ParseUint(port, 10, 16)
+    _, err = strconv.ParseUint(port, 10, 16)
     if err != nil {
-	    log.Printf("Conversion error or invalid port number: %q", err)
+	    log.Fatalf("Conversion error or invalid port number: %q", err)
     }
 
-    api.Router(port)
-    // First server
-    mux1 := http.NewServeMux()
-    mux1.HandleFunc("/", handler1)
+    p := api.Probes{delay_startup, fail_liveness, fail_readiness}
 
-    // Second server
-    mux2 := http.NewServeMux()
-    mux2.HandleFunc("/", handler2)
-
-    go func() {
-        fmt.Println("Server 1 running on :8081")
-        err := http.ListenAndServe(":8081", mux1)
-        if err != nil {
-            panic(err)
-        }
-    }()
-
-    /*
-    fmt.Println("Server 2 running on :9090")
-    err := http.ListenAndServe(":9090", mux2)
-    if err != nil {
-        panic(err)
+    if delay_startup {
+	    time.Sleep(60 * time.Second)
     }
-    */
+    api.Router(port, p)
 }
 
